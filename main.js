80cdb0fc47920baa3f3872b9bb67e623425ca59d
Teamjbworks <116152922+Teamjbworks@users.noreply.github.com>
2023-03-04 16:59:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function validateForm() {
 function insta(){
   window.open("https://instagram.com/the_novus_fortuna?igshid=ZDdkNTZiNTM=");
 }
+function btn1(){
+  window.open("tel:+918606795033") ;
+}
+
+function btn2() {
+  window.open("tel:+919526314256");
+}
